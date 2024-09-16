3f26d3a4757c6c7df5ad11b350b683ae2048a694
Davod <davodnikoyii@gmail.com>
2024-09-16 14:29:21 +0200

    README.md file has been created

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
+import { useBatteryLevel } from 'expo-battery';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const battryLevel = useBatteryLevel(); 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Batterinivå: {battryLevel}</Text>
       <StatusBar style="auto" />
     </View>
   );
